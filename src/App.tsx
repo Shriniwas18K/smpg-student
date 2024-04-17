c099fa95eaea8c4b00daaf0a7f5e4b35c64d6f99
Shriniwas18K <kshriniwas180205@gmail.com>
2024-04-17 21:11:01 +0530

    erter

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,10 @@ import './App.css';
 import OutputCardfield from './Visitor/OutputCardfield';
 const products=[
   ['a1-PG,Tilak Colony,sector-26,akurdi,Pune',2500,"./img1.jpg",10123],
-  ['Rajesh-PG,satpura road,akurdi,Pune',2000,"./img2.jpg",23141],
-  ['Opposite PCCOE campus , Sector-26 , Akurdi',3000,"./img3.jpg",12413],
-  ['Gat-24,sector-27,akurdi,Pune',1000,"",43323]
+  ['Rajesh-PG,satpura road,akurdi,Pune',2000,"./img2.jpeg",23141],
+  ['Opposite PCCOE campus , Sector-26 , Akurdi',3000,"./img3.jpg",12413]
 ]
 function App() {
-  const searchForm = document.querySelector('.search-form');
-  const searchBox = document.querySelector('.search-box');
-  const cardContainer = document.querySelector('.card-container');
-  searchForm?.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const searchTerm = searchBox.value.toLowerCase();
-    Array.from(cardContainer.querySelectorAll('.card')).forEach(card => {
-      const cardContent = card.textContent.toLowerCase();
-      if (cardContent.includes(searchTerm)) {
-        card.style.display = 'block';
-      } else {
-        card.style.display = 'none';
-      }
-    });
-  });
   let content=products.map((item,inasd)=>(
     <OutputCardfield address={item[0]} key={inasd}
     imlink={item[2]} rentPerPerson={item[1]} 
